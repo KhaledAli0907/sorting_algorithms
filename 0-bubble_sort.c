a6c174eec03e16626c4723fd0a1bfe3d4085431e
khaledali0907 <khaledali0907@gmail.com>
2023-11-20 21:36:39 +0200

    task#0

---
+++ 0-bubble_sort.c
@@ -0,0 +1,29 @@
+#include "sort.h"
+
+void bubble_sort(int *array, size_t size) {
+
+
+size_t i = 0, j = 0;
+
+bool swapped = false;
+
+for (i = size - 1; i > 0; i--)
+{
+    for (j = 0; j < i; j++)
+    {
+        if (array[j] > array[j + 1])
+        {
+            int temp = array[j];
+            array[j] = array[j + 1];
+            array[j + 1] = temp;
+
+            swapped = true;
+
+            print_array(array, size);
+        }
+    }
+
+    if (!swapped)
+        break;
+}
+}
